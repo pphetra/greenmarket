5bace1bc0f0e23589c32306bbb57fd072a8472b8
Polawat Phetra <pphetra@gmail.com>
2011-10-07 18:10:15 +0700

    add product 's image

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
     belongs_to :supplier
     belongs_to :category
     belongs_to :standard
+
+    has_attached_file :image, :style => { :thumb => "100x100>" }
 end
